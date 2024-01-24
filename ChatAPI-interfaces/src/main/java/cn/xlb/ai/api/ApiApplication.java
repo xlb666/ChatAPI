9d1c768d7de55ca6ee02f5e9eb408db78649c8de
Little_G <1915900452@qq.com>
2024-01-24 14:56:45 +0800

    初始化ddd架构

---
+++ ChatAPI-interfaces/src/main/java/cn/xlb/ai/api/ApiApplication.java
@@ -0,0 +1,17 @@
+package cn.xlb.ai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动入口
+ *
+ * @author 19159
+ * &#064;created 2024/1/24
+ */
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
---
+++ ChatAPI-interfaces/src/test/java/cn/xlb/ai/test/ApiTest.java
@@ -0,0 +1,10 @@
+package cn.xlb.ai.test;
+
+/**
+ * 单元测试
+ *
+ * @author 19159
+ * &#064;created 2024/1/24
+ */
+public class ApiTest {
+}
